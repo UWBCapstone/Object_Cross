6efc01d89e47a8440c05fadc2a7ced3442f2c550
Thomas-Desktop2017 <junichifujio@gmail.com>
2018-02-19 21:59:09 -0800

    Fix: Adjusted car sizes and verified application operation as intended.
    Issue: Shader for fire wings used aren't working correctly and display as pink.
    Issue: Car is slightly small. May have to scale car model to be approximately .115 instead of .1 in all directions.

---
+++ Object_Cross/Assets/Scripts/camfocus.cs
@@ -0,0 +1,30 @@
+﻿// Script taken from 
+// https://stackoverflow.com/questions/41607773/how-to-change-vuforia-ar-camera-focus-mode
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class camfocus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        VuforiaARController.Instance.RegisterOnPauseCallback(OnPaused);
+	}
+	
+    private void OnVuforiaStarted()
+    {
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if(!paused) // resumed
+        {
+            // Set again autofocus mode when app is resumed
+            CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+    }
+}
